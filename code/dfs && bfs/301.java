b81d8e9edf46a21ecf7c0a54c67c38dbe80c3260
root <2281358393@qq.com>
2020-05-25 19:37:34 +0800

    add 301 0.1

--- code/dfs && bfs/301.java
+++ code/dfs && bfs/301.java
@@ -1,5 +1,6 @@
- class Solution {
+class Solution {
     HashSet<String> set;
+
     public List<String> removeInvalidParentheses(String s) {
         set = new HashSet();
         List<String> res = new ArrayList();
@@ -9,28 +10,34 @@
         }
         set.add(s);
         while (!set.isEmpty()) {
-            for (String tmpS: set) {
+            for (String tmpS : set) {
                 if (isValid(tmpS)) {
                     res.add(tmpS);
                 }
             }
             if (!res.isEmpty()) return res;
-            HashSet<String> nextSet = new HashSet();
-            for (String tmpS: set) {
-                for (int i = 0; i < tmpS.length(); i++) {
-                    if (tmpS.charAt(i) == '(' || tmpS.charAt(i) == ')') {
-                        nextSet.add(tmpS.substring(0, i) + tmpS.substring(i+1, tmpS.length()));
-                    }
-                }
-            }
-            set = nextSet;
+            set = updateSet(set);
+
         }
         return res;
     }
 
+    private HashSet<String> updateSet(HashSet<String> set) {
+        HashSet<String> nextSet = new HashSet();
+        for (String tmpS : set) {
+            for (int i = 0; i < tmpS.length(); i++) {
+                if (tmpS.charAt(i) == '(' || tmpS.charAt(i) == ')') {
+                    nextSet.add(tmpS.substring(0, i) + tmpS.substring(i + 1, tmpS.length()));
+                }
+            }
+        }
+        return nextSet;
+    }
+
+    //  check the the vaild
     private boolean isValid(String s) {
         int countP = 0;
-        for (char a: s.toCharArray()) {
+        for (char a : s.toCharArray()) {
             if (a == '(') countP += 1;
             else {
                 if (a == ')') countP -= 1;
